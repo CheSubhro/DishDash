b92cb6b93e9ee5d8c92325ebf0ae31729a052342
CheSubhro <mitsubhra@gmail.com>
2026-07-23 11:42:21 +0530

    refactor: updated App.js to render RootScreen maintaining a clean separation of concerns and modular architecture

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,13 +1,13 @@
 
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import AppNavigator from './src/navigation/AppNavigator';
+import RootScreen from './src/screens/RootScreen'; 
 import './global.css';
 
 export default function App() {
     return (
         <SafeAreaView className="flex-1 bg-slate-50">
-            <AppNavigator />
+            <RootScreen />
         </SafeAreaView>
     );
 }
